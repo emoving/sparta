7820f451883d97fb555ae82c1e72ffeedd248207
이동주 <leednj0113@naver.com>
2024-06-21 02:27:29 +0900

    feat: 포인트 리포지토리 구현

---
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointHistoryRepository.java
@@ -0,0 +1,10 @@
+package io.hhplus.tdd.point;
+
+import java.util.List;
+
+public interface PointHistoryRepository {
+
+    PointHistory insert(long userId, long amount, TransactionType type, long updateMillis);
+
+    List<PointHistory> selectAllByUserId(long userId);
+}

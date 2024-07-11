dc70b610094caf6b2eeb6147eaafc8845d5c2fca
이동주 <leednj0113@naver.com>
2024-07-12 08:36:56 +0900

    feat: 대기열 스케쥴러 생성

---
+++ reservation-concert/src/main/java/com/hhplus/reservation_concert/global/scheduler/TokenScheduler.java
@@ -0,0 +1,59 @@
+package com.hhplus.reservation_concert.global.scheduler;
+
+import com.hhplus.reservation_concert.domain.token.Token;
+import com.hhplus.reservation_concert.domain.token.TokenStatus;
+import com.hhplus.reservation_concert.infrastructure.token.TokenRepositoryImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TokenScheduler {
+
+    private final TokenRepositoryImpl tokenRepository;
+
+    @Scheduled(fixedRate = 30 * 1000)
+    public void updateTokenStatus() {
+        deleteExpiredTokens();
+        activateWaitingTokens();
+    }
+
+    @Transactional
+    public void deleteExpiredTokens() {
+        long EXPIRE_MINUTE = 30;
+        LocalDateTime expireThreshold = LocalDateTime.now().minusMinutes(EXPIRE_MINUTE);
+        List<Token> activeTokens = tokenRepository.findByStatus(TokenStatus.ACTIVE);
+
+        // EXPIRE_MINUTE 동안 미사용된 토큰 삭제
+        for (Token token : activeTokens) {
+            if (token.getRecentUsedAt().isBefore(expireThreshold)) {
+                tokenRepository.delete(token);
+            }
+        }
+    }
+
+    @Transactional
+    public void activateWaitingTokens() {
+        long LIMIT_USER_COUNT = 10;
+        long activeTokenCount = tokenRepository.countByStatus(TokenStatus.ACTIVE);
+
+        if (activeTokenCount < LIMIT_USER_COUNT) {
+            List<Token> waitingTokens = tokenRepository.findAllByStatusOrderById(TokenStatus.WAITING);
+
+            for (Token token : waitingTokens) {
+                token.setActive();
+                tokenRepository.save(token);
+                activeTokenCount++;
+
+                if (activeTokenCount >= LIMIT_USER_COUNT) {
+                    break;
+                }
+            }
+        }
+    }
+}

62dd25795364265644faf39d12eb9400de5118fb
이동주 <leednj0113@naver.com>
2024-07-25 08:34:58 +0900

    feat: 포인트 차감(낙관적 락)

--- concurrency/src/main/java/com/hhplus/reservation_concert/domain/user/UserService.java
+++ concurrency/src/main/java/com/hhplus/reservation_concert/domain/user/UserService.java
@@ -1,11 +1,12 @@
 package com.hhplus.reservation_concert.domain.user;
 
 import com.hhplus.reservation_concert.domain.user.point.PointHistory;
-import com.hhplus.reservation_concert.global.exception.ErrorCode;
 import com.hhplus.reservation_concert.global.exception.CustomException;
+import com.hhplus.reservation_concert.global.exception.ErrorCode;
 import com.hhplus.reservation_concert.infrastructure.user.PointHistoryRepositoryImpl;
 import com.hhplus.reservation_concert.infrastructure.user.UserRepositoryImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,6 +26,17 @@ public class UserService {
         PointHistory pointHistory = user.charge(point);
 
         return pointHistoryRepository.save(pointHistory);
+    }
+
+    public void usePointWithOptimisticLock(Long id, Integer point) {
+        User user = getUser(id);
+
+        try {
+            PointHistory pointHistory = user.use(point);
+            pointHistoryRepository.save(pointHistory);
+        } catch (OptimisticLockingFailureException e) {
+            throw new CustomException(ErrorCode.POINT_REQUEST_EXIST, e);
+        }
     }
 
     public void usePoint(User user, Integer point) {
